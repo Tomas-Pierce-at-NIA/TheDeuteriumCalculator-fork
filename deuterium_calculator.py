dd5bf05a205d944787b239a3d591e375350eba90
Tomas Pierce <piercetf@nih.gov>
2026-01-29 12:02:41 -0500

    first revision of modification accounting item

--- deuterium_calculator.py
+++ deuterium_calculator.py
@@ -2,6 +2,7 @@
 import os
 os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 
+import re
 import numpy as np
 import pandas as pd
 import csv
@@ -1083,23 +1084,26 @@ class Peptide:
 
     # calculates the average mass from the sequence (Uses values in the PARAMETERS.py file)
     def set_average_mass(self):
-        mass = 0
-        for amino in self._sequence:
-            if amino in CON.PEPTIDE_MASS_DICTIONARY:
-                mass += CON.PEPTIDE_MASS_DICTIONARY[amino]
-            else:
+        # mass = 0
+        # for amino in self._sequence:
+            # if amino in CON.PEPTIDE_MASS_DICTIONARY:
+                # mass += CON.PEPTIDE_MASS_DICTIONARY[amino]
+            # else:
                 
-                breakpoint()
-        mass += CON.MASS_OF_WATER
-        self._average_mass = mass
+                # breakpoint()
+        # mass += CON.MASS_OF_WATER
+        # self._average_mass = mass
+        mass_computer = ModifiedMassComputer()
+        return mass_computer.compute_mass_avg(self._sequence)
 
 class ModifiedMassComputer:
     """
-    parses peptide string with (modified:tag) kind codes and computes their 
+    parses peptide string with (UniMod:tag) kind codes and computes their 
     average mass taking into account the modification
     """
     
     unimod = UniMod.default()
+    mod_matcher = re.compile(r"\(UniMod:(\d+)\)")
     
     def __init__(self, umod=None):
         if umod is not None:
@@ -1107,13 +1111,51 @@ class ModifiedMassComputer:
         else:
             self.uni_db = self.unimod
     
-    def parse(self, peptide :str):
+    def parse_avg(self, peptide :str):
         """
         parse each peptide, whether modified or not,
-        into a generator sequence of corresponding mass values
+        into a generator sequence of corresponding average mass values
         based on the parameters file and the modification database
         """
         curr_idx = 0
+        while curr_idx < len(peptide):
+            sym_here = peptide[curr_idx]
+            if sym_here in CON.PEPTIDE_MASS_DICTIONARY:
+                yield CON.PEPTIDE_MASS_DICTIONARY[sym_here]
+                curr_idx += 1
+            elif sym_here == '(':
+                # get the mass change from the modification
+                end_modtag = peptide.find(')',curr_idx)
+                modtag = peptide[curr_idx:end_modtag+1]
+                mod_match = self.mod_matcher.match(modtag)
+                mod_id = mod_match.groups()[0]
+                modification = self.uni_db[mod_id]
+                mod_avg_mass = modification["avge_mass"]
+                # get the peptide being modified and its mass
+                curr_idx = end_modtag+1
+                pep_sym = peptide[curr_idx]
+                pep_basemass = CON.PEPTIDE_MASS_DICTIONARY[pep_sym]
+                # apply the modification
+                modified_mass = pep_basemass + mod_avg_mass
+                yield modified_mass
+                curr_idx += 1
+            else:
+                raise RuntimeError(
+                    "Encountered unexpected symbol {} during peptide parsing, likely issue in parser".format(
+                        sym_here
+                    )
+                )
+    
+    def compute_mass_avg(self, peptide :str) -> float:
+        """
+        Compute the average mass of a peptide, taking into account 
+        modifications of format (UniMod:modID)
+        """
+        mass = 0
+        for pep_mass in self.parse_avg(peptide):
+            mass += pep_mass
+        mass += CON.MASS_OF_WATER
+        return mass
         
 
 def get_time_points():
