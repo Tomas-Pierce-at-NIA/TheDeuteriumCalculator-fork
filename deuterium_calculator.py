a7d1c3fa45e75928080f5908f533b11306d8cb16
Tomas Pierce <piercetf@nih.gov>
2026-01-29 14:00:32 -0500

    working on fixing running using modified peptides

--- deuterium_calculator.py
+++ deuterium_calculator.py
@@ -940,7 +940,8 @@ class Peptide:
         self._start, self._end = find_start_end(self._sequence, self._protein)
         self._fit = 0  # Gaussian fit
         
-        self.__monoisotopic_mass = mass.fast_mass(sequence)
+        #self.__monoisotopic_mass = mass.fast_mass(sequence)
+        self.__monoisotopic_mass = ModifiedMassComputer().compute_mass_monoisotopic(sequence)
     
     @property
     def monoisotopic_mass(self):
@@ -1111,6 +1112,58 @@ class ModifiedMassComputer:
         else:
             self.uni_db = self.unimod
     
+    def parse_monoisotopic(self, peptide :str):
+        """
+        parse each peptide, whether modified or not
+        into a generator sequence of corresponding mono-isotopic mass values
+        based on pyteomics mass.fast_mass and the UniMod database we carry locally
+        """
+        curr_idx = 0
+        while curr_idx < len(peptide):
+            sym_here = peptide[curr_idx]
+            if sym_here in CON.PEPTIDE_MASS_DICTIONARY:
+                #yield CON.PEPTIDE_MASS_DICTIONARY[sym_here]
+                yield mass.fast_mass(sym_here)
+                curr_idx += 1
+            elif sym_here == '(':
+                # get the mass change from the modification
+                end_modtag = peptide.find(')',curr_idx)
+                modtag = peptide[curr_idx:end_modtag+1]
+                mod_match = self.mod_matcher.match(modtag)
+                mod_id = mod_match.groups()[0]
+                modification = self.uni_db[mod_id]
+                mod_mono_iso_mass = float(modification['mono_mass'])
+                #mod_avg_mass = float(modification["avge_mass"])
+                # get the peptide being modified and its mass
+                curr_idx = end_modtag+1
+                # account for terminal modifications which may be present
+                # after the last peptide
+                if curr_idx < len(peptide):
+                    pep_sym = peptide[curr_idx]
+                    pep_basemass = mass.fast_mass(pep_sym)
+                    #pep_basemass = CON.PEPTIDE_MASS_DICTIONARY[pep_sym]
+                    # apply the modification
+                    modified_mass = pep_basemass + mod_mono_iso_mass
+                    yield modified_mass
+                    curr_idx += 1
+                else:
+                    # no following peptide, but there is a trailing modification which has a mass
+                    yield mod_mono_iso_mass
+            else:
+                raise RuntimeError(
+                    "Encountered unexpected symbol {} during peptide parsing, likely issue in parser".format(
+                        sym_here
+                    )
+                )
+    
+    def compute_mass_monoisotopic(self, peptide :str):
+        mass = 0
+        for pep_mass in self.parse_monoisotopic(peptide):
+            mass += pep_mass
+        # monoisotopic mass of water is also average mass of water
+        mass += CON.MASS_OF_WATER
+        return mass
+    
     def parse_avg(self, peptide :str):
         """
         parse each peptide, whether modified or not,
@@ -1133,12 +1186,18 @@ class ModifiedMassComputer:
                 mod_avg_mass = float(modification["avge_mass"])
                 # get the peptide being modified and its mass
                 curr_idx = end_modtag+1
-                pep_sym = peptide[curr_idx]
-                pep_basemass = CON.PEPTIDE_MASS_DICTIONARY[pep_sym]
-                # apply the modification
-                modified_mass = pep_basemass + mod_avg_mass
-                yield modified_mass
-                curr_idx += 1
+                # account for terminal modifications which may be present
+                # after the last peptide
+                if curr_idx < len(peptide):
+                    pep_sym = peptide[curr_idx]
+                    pep_basemass = CON.PEPTIDE_MASS_DICTIONARY[pep_sym]
+                    # apply modified mass
+                    modified_mass = pep_basemass + mod_avg_mass
+                    yield modified_mass
+                    curr_idx += 1
+                else:
+                    # no following peptide, but there is a modification, which has a mass
+                    yield mod_avg_mass
             else:
                 raise RuntimeError(
                     "Encountered unexpected symbol {} during peptide parsing, likely issue in parser".format(
@@ -1233,11 +1292,13 @@ def get_file_name(time_points, is_differential, num_free_replications, num_compl
     return pattern_list
 
 def set_average_mass(sequence):
-    mass = 0
-    for amino in sequence:
-        mass += CON.PEPTIDE_MASS_DICTIONARY[amino]
-    mass += CON.MASS_OF_WATER
-    return mass
+    computer = ModifiedMassComputer()
+    return computer.compute_mass_avg(sequence)
+    # mass = 0
+    # for amino in sequence:
+        # mass += CON.PEPTIDE_MASS_DICTIONARY[amino]
+    # mass += CON.MASS_OF_WATER
+    # return mass
 
 def weighted_average_mass_mlf(mz, Intensity, charge, average_mass):
     mz = [float(i) for i in mz]
